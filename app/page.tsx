f07e31e02dfbc2dd27f601814fc96291f2baf7e4
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2025-06-01 19:24:33 +0800

    Adjustments: remove other contents other than header

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import HomeEntrySection from "@/components/home/entry";
+// import HomeEntrySection from "@/components/home/entry";
 import HomeHeroSection from "@/components/home/hero";
 
 export default function Home() {
   return (
     <>
         <HomeHeroSection />
-        <HomeEntrySection />
+        {/* <HomeEntrySection />
         <section className="h-[100vh] bg-neutral-100">
             <div className="container">
                 <h1 className="text-center text-4xl mb-20"> Welcome to Gallery </h1>
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nisi vitae ab illo at debitis nostrum voluptatum mollitia unde autem, repellat iure quisquam odit, nemo sit. Repellat adipisci laborum veniam, amet laboriosam esse magni excepturi. Sequi fugiat officia deleniti distinctio vel nostrum ipsam, aliquam labore sapiente ipsa maxime. Deleniti cupiditate hic porro natus quibusdam delectus quaerat. Maxime, soluta dolore libero tempore expedita aliquam laboriosam dolores rem doloribus sed iure eius. Voluptate possimus ut aliquam in rerum itaque suscipit corporis accusamus rem architecto saepe, eligendi iusto beatae alias non, deleniti commodi similique. Et iure incidunt facere, cum rerum impedit animi, ut modi harum iste reiciendis. Eveniet, porro. Facilis pariatur dolorum ullam saepe quia voluptatem nihil numquam officia tempora libero distinctio quo ipsam veritatis voluptas, assumenda iste! Sint laborum, cumque excepturi corporis aliquid, obcaecati id non nisi est alias voluptatibus pariatur in? Hic, voluptatum sint esse ducimus, quaerat consectetur odit amet quia sed molestias rem nesciunt! Placeat nobis, repellendus consequatur dicta laborum expedita eaque neque maiores laudantium amet esse repellat nemo ullam ea voluptatem laboriosam vitae. Perferendis voluptates libero nisi eaque accusantium dolores. Fugiat non maxime quam officiis? Cum delectus blanditiis reiciendis ex quisquam. Blanditiis error veniam unde pariatur velit eligendi maiores. </p>
             </div>
-        </section>
+        </section> */}
     </>
   );
 }
